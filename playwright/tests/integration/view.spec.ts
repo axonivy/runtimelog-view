7de5465b122b505137f6cb2b0b487f0dd05d9689
ivy-tgr <tobias.gretler@axonivy.com>
2025-01-21 13:32:21 +0100

    refactor: reorganize project structure; remove unused files and update YAML schemas

--- playwright/tests/integration/view.spec.ts
+++ playwright/tests/integration/view.spec.ts
@@ -1,7 +1,12 @@
 import { expect, test } from '@playwright/test';
 import { LogView } from '../page-objects/log-view';
 
-test('Hello World', async ({ page }) => {
+test('Hello World Mock', async ({ page }) => {
   const editor = await LogView.openMock(page);
+  expect(editor.page.getByRole('heading', { name: 'Hello World' }).first()).toContainText("Hello World");
+});
+
+test('Hello World', async ({ page }) => {
+  const editor = await LogView.open(page);
   expect(editor.page.getByRole('heading', { name: 'Hello World' }).first()).toContainText("Hello World");
 });
