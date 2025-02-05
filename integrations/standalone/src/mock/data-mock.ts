d1e24a2ba61c0e03962edbd7210e5ceedc2dcdf4
ivy-tgr <tobias.gretler@axonivy.com>
2025-02-05 12:04:54 +0100

    XIVY-15692 Refactor runtime Log View

--- integrations/standalone/src/mock/data-mock.ts
+++ integrations/standalone/src/mock/data-mock.ts
@@ -1,24 +1,94 @@
-import type { RuntimeLogEntryLsp, Level } from '@axonivy/log-view-protocol';
+import type { RuntimeLogViewData, Level } from '@axonivy/log-view-protocol';
 
 const LevelValues: { [key in Level]: Level } = {
-  OFF: "OFF",
-  FATAL: "FATAL",
-  ERROR: "ERROR",
-  WARN: "WARN",
-  INFO: "INFO",
-  DEBUG: "DEBUG",
-  TRACE: "TRACE",
-  ALL: "ALL"
+  OFF: 'OFF',
+  FATAL: 'FATAL',
+  ERROR: 'ERROR',
+  WARN: 'WARN',
+  INFO: 'INFO',
+  DEBUG: 'DEBUG',
+  TRACE: 'TRACE',
+  ALL: 'ALL'
 };
 
-export const data: RuntimeLogEntryLsp = {
-  id: '1',
-  level: LevelValues.INFO,
-  message: 'Hello World',
-  category: 'test',
-  processElement: '',
-  request: '',
-  throwableInformationMsg: '',
-  timestamp: '',
-  userDialogId: ''
+export const data: RuntimeLogViewData = {
+  entries: [
+    {
+      level: LevelValues.INFO,
+      message: 'Process intermediate event',
+      category: 'User',
+      processElement: '',
+      request: '',
+      throwableInformationMsg: '',
+      timestamp: 'Wed Jan 22 16:11:52 CET 2025',
+      userDialogId: 'null',
+      project: 'portal'
+    },
+    {
+      level: LevelValues.WARN,
+      message: 'Process intermediate event',
+      category: 'User',
+      processElement: '',
+      request: '',
+      throwableInformationMsg: '',
+      timestamp: 'Wed Jan 22 16:11:52 CET 2025',
+      userDialogId: 'null',
+      project: 'portal'
+    },
+    {
+      level: LevelValues.ERROR,
+      message: 'Process intermediate event',
+      category: 'User',
+      processElement: '',
+      request: '',
+      throwableInformationMsg: '',
+      timestamp: 'Wed Jan 22 16:11:52 CET 2025',
+      userDialogId: 'null',
+      project: 'portal'
+    },
+    {
+      level: LevelValues.ERROR,
+      message: 'Process intermediate event',
+      category: 'runtimelog',
+      processElement: '',
+      request: '',
+      throwableInformationMsg: '',
+      timestamp: 'Wed Jan 22 16:11:52 CET 2025',
+      userDialogId: 'null',
+      project: 'portal'
+    },
+    {
+      level: LevelValues.ERROR,
+      message: 'Process intermediate event',
+      category: 'runtimelog',
+      processElement: '',
+      request: '',
+      throwableInformationMsg: '',
+      timestamp: 'Wed Jan 22 16:11:52 CET 2025',
+      userDialogId: 'null',
+      project: 'Process-inspector'
+    },
+    {
+      level: LevelValues.FATAL,
+      message: 'Process intermediate event',
+      category: 'USer',
+      processElement: '',
+      request: '',
+      throwableInformationMsg: '',
+      timestamp: 'Wed Jan 22 16:11:52 CET 2025',
+      userDialogId: 'null',
+      project: 'Process-inspector'
+    },
+    {
+      level: LevelValues.DEBUG,
+      message: 'Process intermediate event',
+      category: 'User',
+      processElement: '',
+      request: '',
+      throwableInformationMsg: '',
+      timestamp: 'Wed Jan 22 16:11:52 CET 2025',
+      userDialogId: 'null',
+      project: 'Process-inspector'
+    }
+  ]
 };
