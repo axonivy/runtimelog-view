4a89afbe4571f64f02f0bd544fd369197284f9b3
ivy-tgr <tobias.gretler@axonivy.com>
2025-05-12 14:48:28 +0200

    XIVY-16611 Test reload data

--- integrations/standalone/src/mock/log-client-mock.ts
+++ integrations/standalone/src/mock/log-client-mock.ts
@@ -1,20 +1,17 @@
 import type { LogClient, RuntimeLogEntry } from '@axonivy/log-view-protocol';
 import { Emitter } from '@axonivy/jsonrpc';
-import { data } from './data-mock';
+import { generateMockData } from './data-mock';
 
 export class LogClientMock implements LogClient {
-  private logData: RuntimeLogEntry[] = data;
-
   protected onValidationChangedEmitter = new Emitter<void>();
   onValidationChanged = this.onValidationChangedEmitter.event;
   protected onDataChangedEmitter = new Emitter<void>();
   onDataChanged = this.onDataChangedEmitter.event;
 
   data(): Promise<RuntimeLogEntry[]> {
-    return Promise.resolve(this.logData);
+    return Promise.resolve(generateMockData());
   }
 
   clear(): void {
-    this.logData = [];
   }
 }
